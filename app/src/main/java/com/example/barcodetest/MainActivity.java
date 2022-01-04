23d249f1799fd94a19a66eee8898c16971eb5fd8
Sungallen <a0975671396@gmail.com>
2022-01-04 16:44:18 +0800

    first

---
+++ app/src/main/java/com/example/barcodetest/MainActivity.java
@@ -0,0 +1,107 @@
+package com.example.barcodetest;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    SurfaceView surfaceView;
+    TextView textView;
+    CameraSource cameraSource;
+    BarcodeDetector barcodeDetector;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getPermissionCamera();
+        surfaceView=(SurfaceView)findViewById(R.id.surface_view);
+        textView=(TextView)findViewById(R.id.barcode_text);
+
+        barcodeDetector = new BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.QR_CODE).build();
+        cameraSource=new CameraSource.Builder(this,barcodeDetector)
+                .setRequestedPreviewSize(300,300).build();
+
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback(){
+
+            @Override
+            public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
+                if(ActivityCompat.checkSelfPermission(getApplicationContext(),Manifest.permission.CAMERA)
+                        !=PackageManager.PERMISSION_GRANTED)
+                    return;
+                try{
+                    cameraSource.start(surfaceHolder);
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
+                cameraSource.stop();
+            }
+
+
+        });
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>(){
+
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(@NonNull Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode>qrCodes=detections.getDetectedItems();
+                if(qrCodes.size()!=0){
+                    textView.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            textView.setText(qrCodes.valueAt(0).displayValue);
+                        }
+                    });
+                }
+            }
+        });
+
+    }
+    public void getPermissionCamera(){
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED){
+            new AlertDialog.Builder(this)
+                    .setCancelable(false)
+                    .setTitle("需要相機權限")
+                    .setMessage("你是不是傻？沒相機我掃雞雞喔？")
+                    .setPositiveButton("知道了拉", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.CAMERA},1);
+                        }
+                    })
+                    .show();
+        }
+
+    }
+}
